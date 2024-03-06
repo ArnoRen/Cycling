322f93a878f1c0a2ec60f764a9f3eaa56d0327fe
ArnoRen <161221311+ArnoRen@users.noreply.github.com>
2024-03-06 20:35:21 +0000

    Tester_Riders eklendi.

---
+++ Coursework/TestSystem/Tester_Riders.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+import cycling.CyclingPortalImpl;
+import cycling.IDNotRecognisedException;
+import cycling.IllegalNameException;
+import cycling.InvalidNameException;
+import cycling.MiniCyclingPortal;
+
+public class Tester_Riders {
+
+    public static void main(String[] args) {
+        
+        MiniCyclingPortal portal = new CyclingPortalImpl();
+
+        try {
+            System.out.println(portal.createTeam("Porsche Peniske","Porsche 963"));
+        } catch (IllegalNameException e) {
+            e.printStackTrace();
+        } catch (InvalidNameException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            System.out.println(portal.createTeam("AF Course","499P"));
+        } catch (IllegalNameException e) {
+            e.printStackTrace();
+        } catch (InvalidNameException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            System.out.println(portal.createTeam("AF Course","499P"));
+        } catch (IllegalNameException e) {
+            e.printStackTrace();
+        } catch (InvalidNameException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(Arrays.toString(portal.getTeams()));
+
+        try {
+            portal.removeTeam(2);
+        } catch (IDNotRecognisedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(Arrays.toString(portal.getTeams()));
+    }
+    
+}
