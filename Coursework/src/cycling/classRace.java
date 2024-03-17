77aecf416b6a0665fa8b3213dad9d2a6152f0e78
ArnoRen <161221311+ArnoRen@users.noreply.github.com>
2024-03-17 20:15:13 +0000

    .

--- Coursework/src/cycling/classRace.java
+++ Coursework/src/cycling/classRace.java
@@ -1,45 +1,19 @@
 package cycling;
-import java.time.LocalDateTime;
 
 public class classRace {
     
   private String name;
-  private double length;
-  private int stageID;
   private int raceID;
-  private double location;
-  private int checkpointID;
   private String description;
-  private LocalDateTime startTime;
-  private StageType stageType;
-  private CheckpointType checkpointType;
-  private Double averageGradient;
 
-  public classRace(String name, double length, int stageID, int raceID, double location, int checkpointID, String description, LocalDateTime startTime, StageType stageType, CheckpointType checkpointType, Double averageGradient) {
+  public classRace(String name, int raceID, String description) {
     this.name = name; 
-    this.length = length;
-    this.stageID = stageID;
-    this.stageType = stageType;
     this.raceID = raceID;
-    this.location = location;
-    this.checkpointID = checkpointID;
     this.description = description;
-    this.startTime = startTime;
-    this.stageType = stageType;
-    this.checkpointType = checkpointType;
-    this.averageGradient = averageGradient;
   }
 
   public String getStageName() {
     return name; 
-  }
-
-  public double getLength() {
-    return length;
-  }
-
-  public int getStageID() {
-    return stageID;
   }
 
   public String getDescription() {
@@ -48,29 +22,5 @@ public class classRace {
 
   public int getRaceID() {
     return raceID;
-  }
-
-  public double getLocation() {
-    return location;
-  }
-
-  public int getCheckpointID() {
-    return checkpointID;
-  }
-
-  public LocalDateTime getStartTime() {
-    return startTime;
-  }
-
-  public StageType getStageType() {
-    return stageType;
-  }
-
-  public CheckpointType getCheckpointTypeEnum() {
-    return checkpointType;
-  }
-
-  public Double getAverageGradient() {
-    return averageGradient;
   }
 }
